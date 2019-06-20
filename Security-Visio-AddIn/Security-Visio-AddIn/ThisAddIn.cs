ffcba68d198020a06ffcdafc60ce8215c1a47d8b
Darquisa <m.lehmann@live.de>
2019-06-20 12:04:52 +0200

    könnt ihr das sehen?

--- Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
+++ Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
@@ -13,6 +13,7 @@ namespace Security_Visio_AddIn
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //Hier könnte Ihr Code stehen
+            //könnt ihr das sehen?
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
