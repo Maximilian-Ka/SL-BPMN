e3de4d1bc35c721cb40e691e31b2b799070e4468
SuicideToast <elmer.janis@gmail.com>
2019-06-20 09:23:04 +0200

    id

--- Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
+++ Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
@@ -12,7 +12,7 @@ namespace Security_Visio_AddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            //test
+            //dfsd
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
