fa92d257d68dd74f8200c881628854e312e6ab42
Darquisa <m.lehmann@live.de>
2019-06-20 12:15:46 +0200

    test in Kommentar

--- Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
+++ Security-Visio-AddIn/Security-Visio-AddIn/ThisAddIn.cs
@@ -13,7 +13,7 @@ namespace Security_Visio_AddIn
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //Hier könnte Ihr Code stehen
-            test
+            //test
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
